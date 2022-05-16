b7d9db5612d64eb890518ab64fb37000c95c1b50
satofumiya <sato.workfree@gmail.com>
2022-05-16 21:03:27 +0900

    vue.js install

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,14 +8,10 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
 
-  console.log(app)
-})
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('this is hello_vue.js')
+});
 
 
 // The above code uses Vue without the compiler, which means you cannot
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,16 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+     new VueLoaderPlugin()
+)
+
+ environment.loaders.prepend('vue', {
+     test: /\.vue$/,
+     use: [{
+         loader: 'vue-loader'
+     }]
+ })
+
+  module.exports = environment
